fd80fe9a267dae4611c6d19e8890b827f2491203
code0xff <ian.jungyong.um@gmail.com>
2023-05-30 20:38:15 +0900

    feat: Change WebAuthnSigner constructor arg format

---
+++ src/WebAuthnSigner.spec.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest';
+import { WebAuthnSigner } from './WebAuthnSigner.js'
+import { TypeRegistry, Binary, UniversalAddress } from '@pinot/types';
+import { u8aToHex } from '@pinot/util';
+
+describe('WebAuthnSigner', (): void => {
+  describe('construct', (): void => {
+    it('can construct', (): void => {
+      const registry = new TypeRegistry();
+      registry.register({
+        'AccountId': 'UniversalAddress',
+        Binary,
+        UniversalAddress,
+      });
+
+      const credentialId = 'vlinaQqUA5Elff-7mKulzg';
+      const publicKey = 'ugCQDyDOUCZ4w6uVaQ6cjDapARUknVRX-09qqYiyYMEC09i8';
+
+      const signer = new WebAuthnSigner(registry, credentialId, publicKey);
+      expect(signer.address).toBe(publicKey);
+      expect(u8aToHex(signer.addressRaw)).toBe('0x8c802403c83394099e30eae55a43a7230daa404549275515fed3daaa622c983040b4f62f');
+    });
+  });
+});
