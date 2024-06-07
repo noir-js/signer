8d4c661df672af05429c0e11451c7b15777f1453
Jeeyong Um <conr2d@proton.me>
2024-06-07 19:32:25 +0800

    feat: Sign to generate recoverable compact signature (#8)
    * build(deps): Add @noble/curves dependency
    
    * feat: Sign to generate recoverable compact signature
    
    * chore: Bump version to 0.3.0

--- src/WebAuthnSigner.spec.ts
+++ src/WebAuthnSigner.spec.ts
@@ -1,24 +1,22 @@
 import { describe, expect, it } from 'vitest';
 import { WebAuthnSigner } from './WebAuthnSigner.js'
-import { TypeRegistry, Binary, UniversalAddress } from '@pinot/types';
-import { u8aToHex } from '@pinot/util';
+import { TypeRegistry, Binary } from '@pinot/types';
+import { decodeAddress, u8aToHex } from '@pinot/util';
 
 describe('WebAuthnSigner', (): void => {
   describe('construct', (): void => {
     it('can construct', (): void => {
       const registry = new TypeRegistry();
       registry.register({
-        'AccountId': 'UniversalAddress',
         Binary,
-        UniversalAddress,
       });
 
       const credentialId = 'vlinaQqUA5Elff-7mKulzg';
-      const publicKey = 'ugCQDyDOUCZ4w6uVaQ6cjDapARUknVRX-09qqYiyYMEC09i8';
+      const accountId = '5HGeP4pgVYAGof7ZUVLmAurkRRFSLZQ5JSB49FMAEKWUUTbF';
 
-      const signer = new WebAuthnSigner(registry, credentialId, publicKey);
-      expect(signer.address).toEqual(publicKey);
-      expect(u8aToHex(signer.addressRaw)).toEqual('0x802403c83394099e30eae55a43a7230daa404549275515fed3daaa622c983040b4f62f');
+      const signer = new WebAuthnSigner(registry, credentialId, accountId);
+      expect(signer.address).toEqual(accountId);
+      expect(u8aToHex(signer.addressRaw)).toEqual('0xe6486944aec720fb45fc1fd69444107128f516a7ebb7fd279f5f25dc4c1f5d14');
     });
   });
 });
